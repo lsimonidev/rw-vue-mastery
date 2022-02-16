570cb4edc122c2e69f117fab08bb0a7dc4f4b0bf
lsimonidev <laura.simoni@gmail.com>
2022-02-16 04:58:16 +0000

    Real World Vue3 - vuemastery - lesson 5

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Event List</router-link> |
+      <router-link to="/">Events</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
